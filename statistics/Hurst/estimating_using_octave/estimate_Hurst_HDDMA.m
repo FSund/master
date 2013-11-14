563aa6036286b4a5dd14305ac39af4ff61e5bdb9
FSund <filipsu@fys.uio.no>
2013-11-14 11:44:55 +0100

    Added n_min as parameter

--- statistics/Hurst/estimating_using_octave/estimate_Hurst_HDDMA.m
+++ statistics/Hurst/estimating_using_octave/estimate_Hurst_HDDMA.m
@@ -1,12 +1,55 @@
 
 % estimate_Hurst_HDDMA.m
 
-function [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(f, n_max, theta, show_plot)
+function [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(f, n_min, n_max, theta, show_plot)
 
 %
 % Higher-Dimensional Detrending Moving Average
 % See "Algorithm to estimate the Hurst exponent of high-dimensional fractals", Anna Carbone, 2007, http://arxiv.org/abs/0711.2892
 % and "Detrending moving average algorithm for multifractals", Gao-Feng Gu and Wei-Xing Xhou, 2010, http://arxiv.org/abs/1005.0877v2
+%
+%
+% The procedure [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(f, n_max, theta, show_plot)
+% is used to calculate the Hurst exponent of two-dimensional data (for example a heightmap).
+%
+% Input:
+%   f: the two-dimensional data
+%   n_min: the lower bound of the segment size n
+%   n_max: the upper bound of the segment size n
+%   theta: the position parameter of the moving window
+%
+% Output:
+%   H: Hurst exponent
+%   nvec: vector with segment sizes
+%   sigma_DMA_squared: variance
+%
+%%% FROM Gu2010 source code (se arxiv.org) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The procedure works as follows:
+%   1) For each n, construct the cumulative sum Y in a moving window.
+%   2) Calculate the moving average function \widetilde{Y}.
+%   3) Determine the residual e by detrending \widetilde{Y} from Y.
+%   4) Estimate the root-mean-square function F.
+%   5) Calculate the q-th order overall fluctuation function Fq.
+%   6) Calculate the multifractal scaling exponent tau(q).
+%   7) Calculate the singularity strength function alpha(q) and spectrum f(alpha).
+%
+% Note:
+%   2) The lower bound n_min would better be selected around 10.
+%   3) The upper bound n_max would better be selected around 10% of min(size(X)).
+%   4) N would better be seleceted in the range [20,40].
+%   5) The parameter theta varies in the range [0,1], and we have
+%      theta=theta_1=theta_2. Theta = 0 corresponds to backward MFDMA, and
+%      theta = 0.5 corresponds to the centered MFDMA, and theta = 1 corresponds to
+%      the forward MFDMA. We recommend theta=0.
+%   6) In the procedure, we have n=n_1=n_2 for the segment size.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Example:
+% 1D:
+%   [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(rand(1,1000), 10, 20, 0.0)
+% 2D:
+%   [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(rand(100,100), 2, 10, 0.0)
 %
 
 if nargin < 4
@@ -37,7 +80,7 @@ if (n_max/N > 0.1)
     disp(['n_max should be << N = ' num2str(N)]);
     return;
 end
-n_min = 2;
+% n_min = 2;
 nvec = n_min:n_max;
 
 sigma_DMA_squared = zeros(1,length(nvec));
@@ -81,6 +124,7 @@ if dim==2
     dim
     for k = 1:length(nvec) % loop over window sizes, using square windows (n1 == n2)
         n = nvec(k);
+        n
 
         % See Gu2010, equation (2) for these ranges
         m_lower = ceil((n-1)*(1-theta));
