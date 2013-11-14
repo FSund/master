e4c462fc0e45716ff1e3847cc70ac88ca653f6b1
FSund <filipsu@fys.uio.no>
2013-11-14 11:05:12 +0100

    Simplified loops, implemented ceil and floor limits from Gu2010 in 1D case.

--- statistics/Hurst/estimating_using_octave/estimate_Hurst_HDDMA.m
+++ statistics/Hurst/estimating_using_octave/estimate_Hurst_HDDMA.m
@@ -1,12 +1,17 @@
 
-% GFGU_MFDMA_2D.m
+% estimate_Hurst_HDDMA.m
 
-function [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(f, n_max, theta)
+function [H, nvec, sigma_DMA_squared] = estimate_Hurst_HDDMA(f, n_max, theta, show_plot)
 
 %
 % Higher-Dimensional Detrending Moving Average
+% See "Algorithm to estimate the Hurst exponent of high-dimensional fractals", Anna Carbone, 2007, http://arxiv.org/abs/0711.2892
+% and "Detrending moving average algorithm for multifractals", Gao-Feng Gu and Wei-Xing Xhou, 2010, http://arxiv.org/abs/1005.0877v2
 %
 
+if nargin < 4
+    show_plot = false;
+end
 
 dim = sum(size(f) > 1);
 if (dim > 2)
@@ -29,7 +34,7 @@ end
 
 N = length(f);
 if (n_max/N > 0.1)
-    disp('n_max should be << N');
+    disp(['n_max should be << N = ' num2str(N)]);
     return;
 end
 n_min = 2;
@@ -38,46 +43,55 @@ nvec = n_min:n_max;
 sigma_DMA_squared = zeros(1,length(nvec));
 if dim==1
     %% 1d %%
-    for n = nvec
-        m = floor(n*theta); % integer part of positive number
-        if (theta == 1.0)
-            start = n-m+1;
-        else
-            start = n-m;
-        end
-        sigma_DMA_squared(n-n_min+1) = 0;
-        for i = start:(N-m) % +1 because matlab starts at 1
-            imax = i+m;
-            imin = imax - n+1;
+    dim
+    for k = 1:length(nvec)
+        n = nvec(k);
+
+        % See Gu2010, equation (2) for these ranges
+        m_lower = ceil((n-1)*(1-theta));
+        m_upper = -floor((n-1)*theta);
+
+        sigma_DMA_squared(k) = 0;
+        for i = (m_lower+1):(N+m_upper) % +1 because matlab starts at 1
+            imax = i - m_upper;
+            imin = i - m_lower;
+            % i
+            % imax
+            % imin
             moving_average = mean( f(imin:imax) );
-            sigma_DMA_squared(n-n_min+1) = sigma_DMA_squared(n-n_min+1) + (f(i) - moving_average)^2;
+            % moving_average
+            sigma_DMA_squared(k) = sigma_DMA_squared(k) + (f(i) - moving_average)^2;
         end
-        sigma_DMA_squared(n-n_min+1) = sigma_DMA_squared(n-n_min+1)*(1/(N-n_max)^dim); % n-n_min+1 to start at first index in vector
+        sigma_DMA_squared(k) = sigma_DMA_squared(k)/(N-n_max);
     end
 
-    x = log(dim.*(nvec.*nvec));
+    x = log(nvec.*nvec);
     y = log(sigma_DMA_squared);
-
-    figure;
-    plot(x, y)
-    hold all;
     fit = polyfit(x, y, 1);
     H = fit(1);
-    plot(x, polyval(fit, x), 'r-')
-    title(['1d, theta = ' num2str(theta)])
-    legend(['H = ' num2str(H)])
+
+    if show_plot
+        figure;
+        plot(x, y)
+        hold all;
+        plot(x, polyval(fit, x), 'r-')
+        title(['1d, theta = ' num2str(theta)])
+        legend(['H = ' num2str(H)])
+    end
 end
 
 if dim==2
     %% 2d %%
-    for n = nvec % loop over window sizes, using square windows (n1 == n2)
+    dim
+    for k = 1:length(nvec) % loop over window sizes, using square windows (n1 == n2)
+        n = nvec(k);
         m = floor(n*theta);
         if (theta == 1.0)
             start = n-m+1;
         else
             start = n-m;
         end
-        sigma_DMA_squared(n-n_min+1) = 0;
+        sigma_DMA_squared(k) = 0;
         for i = start:(N-m) % loop over window positions
             imax = i+m;
             imin = imax - n+1;
@@ -91,21 +105,23 @@ if dim==2
                 %       the error is of the size e-16. mean(mean()) is probably faster!
                 % moving_average = sum(sum(f(imin:imax, jmin:jmax)))/(n^2);
 
-                sigma_DMA_squared(n-n_min+1) = sigma_DMA_squared(n-n_min+1) + (f(i,j) - moving_average)^2;
+                sigma_DMA_squared(k) = sigma_DMA_squared(k) + (f(i,j) - moving_average)^2;
             end
         end
-        sigma_DMA_squared(n-n_min+1) = sigma_DMA_squared(n-n_min+1)*(1/(N-n_max)^dim);
+        sigma_DMA_squared(k) = sigma_DMA_squared(k)*(1/(N-n_max)^dim);
     end
 
     x = log(dim.*(nvec.*nvec));
     y = log(sigma_DMA_squared);
-
-    figure;
-    plot(x, y)
-    hold all;
     fit = polyfit(x, y, 1);
     H = fit(1);
-    plot(x, polyval(fit, x), 'r-')
-    title(['2d, theta = ' num2str(theta)])
-    legend(['H = ' num2str(H)])
+
+    if show_plot
+        figure;
+        plot(x, y)
+        hold all;
+        plot(x, polyval(fit, x), 'r-')
+        title(['2d, theta = ' num2str(theta)])
+        legend(['H = ' num2str(H)])
+    end
 end
