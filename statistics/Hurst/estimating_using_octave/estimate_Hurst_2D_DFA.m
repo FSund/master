fe0ac2894df5586cd6f4ae947f8df3f4096010a7
FSund <filipsu@fys.uio.no>
2014-02-10 09:54:58 +0100

    In Hurst: cleaned up 2D_DFA.

--- statistics/Hurst/estimating_using_octave/estimate_Hurst_2D_DFA.m
+++ statistics/Hurst/estimating_using_octave/estimate_Hurst_2D_DFA.m
@@ -1,35 +1,9 @@
 function estimate_Hurst_2D_DFA(X)
 
-%     function ret = cumsum2(X)
-%         [m,n] = size(X);
-%         ret = zeros(m,n);
-%         for i=1:m
-%             for j=1:n
-%                 sum = 0.0;
-%                 for k=1:i
-%                     for l=1:j
-%                         sum = sum + X(k,l);
-%                     end
-%                 end
-%                 ret(i,j) = sum;
-%             end
-%         end
-%         return
-%     end
-% 
-%     a = rand(10,10);
-%     b = cumsum(cumsum(a,2),1)
-%     c = cumsum2(a)
-%     d = abs(b-c) > 1e-10
-% 
-%     return
-
     [M,N] = size(X);
     smin = 6;                       % approx, can be tuned
     smax = floor(min([M,N])/4.0);   % approx, can be tuned
     F2 = cell(1, length(smin:smax));
-
-%     svec = smax/4;
     svec = smin:smax;
 
     s_counter = 1;
@@ -39,43 +13,14 @@ function estimate_Hurst_2D_DFA(X)
         Fvws2 = zeros(Ms, Ns);
         for v = 1:Ms % loop over segments
             for w = 1:Ns % loop over segments
-%                 ivec = ((v-1)*s + 1):(v*s);
                 ivec = (v-1)*s + (1:s);
-%                 jvec = ((w-1)*s + 1):(w*s);
                 jvec = (w-1)*s + (1:s);
-%                 Xuv = X(ivec, jvec);
+%                 Xuv = X(ivec, jvec); % not needed
                 
-                % Auto cumulative sum
+                % Cumulative sum
                 u = cumsum(cumsum(X(ivec, jvec),2),1); % 1 in the end is default
-%                 u = cumsum(cumsum(X(ivec, jvec),1),2); % equivalent to above
-%                 u = cumsum2(X(ivec, jvec));
                 
-%                 % Manual cumulative sum, eq. (1)
-%                 % ! Tested -- Equivalent to the above !
-%                 u = zeros(s,s);
-%                 x = X(ivec, jvec);
-%                 for i=1:s
-%                     for j=1:s
-% %                         sum_ = 0.0;
-% %                         for ii=1:i
-% %                             for jj=1:j
-% %                                 sum_ = sum_ + x(ii,jj);
-% %                             end
-% %                         end
-% %                         u(i,j) = sum_;
-%                         u(i,j) = sum(sum(x(1:i, 1:j)));
-%                     end
-%                 end
-                
-                % Fit to different polynomials {\tilde u}
-                
-                % polynomial: z = a1*x + a2*x^2 + a3*x^2*y + a4*y^2 etc.
-                % the coefficients a1,a2 etc. are given by vector A, defined as
-                % A=[x x.^2 x.^2.*y y.^2 etc.]\z,
-                % so four our problem we get for polynomial in eq (2)
-                % u(i,j) = ai + bj + c
-                % --> z = ax + by + c
-                
+                % Fit to different polynomials              
                 [xx,yy] = meshgrid(ivec, jvec);
                 x = xx(:); % Reshape/create 1d vectors
                 y = yy(:);
@@ -93,7 +38,6 @@ function estimate_Hurst_2D_DFA(X)
                 
                 % Find residual matrix eps_{u,v}
                 eps = u - U;
-%                 mean(mean(eps))
 
                 % % Debug stuff % %
 %                 figure(3)
@@ -114,100 +58,24 @@ function estimate_Hurst_2D_DFA(X)
             end
         end
         F2{s_counter} = Fvws2;
-%         Fvws2
         s_counter = s_counter + 1;
     end
 
     svec = smin:smax;
     
     % Overall detrended fluctuation, eq. (9)
-    % Auto
-    F2_auto = cellfun(@mean2, F2);
-
-    % Manual
-%     % ! Tested -- F2_mean equal to auto above, within 1e-7 !
-%     F_mean = zeros(size(svec));
-%     F2_mean = zeros(size(svec));
-%     ii = 1;
-%     for s = svec
-%         Ms = floor(M/s);
-%         Ns = floor(N/s);
-%         
-%         sum_ = 0.0;
-%         for v = 1:Ms
-%             for w = 1:Ns
-%                 sum_ = sum_ + F2{ii}(v,w);
-%             end
-%         end
-%         F_mean(ii) = sqrt(sum_/(Ms*Ns));
-%         F2_mean(ii) = sum_/(Ms*Ns);
-%         
-%         ii = ii+1;
-%     end
-
-    FF = F2_auto;
+    F2 = cellfun(@mean2, F2);
 
     x = log10(svec);
-    y = (1/5.0)*log10(FF);
-%     y = log10(sqrt(FF));
+    y = log10(sqrt(F2));
     p = polyfit(x,y,1);
-    p
+    fprintf('H = %1.2f\n', p(1));
 
     figure;
-    plot(log10(svec), log10(sqrt(FF)));
-    hold all;
-%     loglog(log10(svec), log10(sqrt(sqrt(FF))));
     plot(x, y);
+    hold all;
     plot(log10([6 2e2]), log10([1.5e-1 2e0]));
 %     plot(log10([6 2e2]), log10([1.5e-1 2e0]*10));
 %     axis([5 6e2 1e-1 5e1]);
-    title('log10(x) log10(y)')
-    legend('log10(sqrt(FF))', 'custom', 'article');
-    
-    
-    
-    
-    
-    
-%     figure;
-%     loglog(svec, F);
-%     axis([5 6e2 1e-1 5e1])
-%     x = log10(svec);
-%     y = log10(F);
-%     figure; plot(x,y);
-%     p = polyfit(x,y,1)
-%     p = polyfit(y,x,1)
-    
-    
-%     figure;
-%     loglog(svec, sqrt(FF));
-%     hold all;
-%     loglog(svec, sqrt(sqrt(FF)));
-%     plot([6 2e2], [1.5e-1 2e0]);
-%     plot([6 2e2], [1.5e-1 2e0]*10);
-% %     axis([5 6e2 1e-1 5e1]);
-%     title('loglog')
-% %     legend('sqrt(FF)', 'sqrt(sqrt(FF))', 'article 1', 'article 2');
-    
-%     figure;
-%     plot(svec,sqrt(FF));
-%     hold all;
-%     plot(svec,sqrt(sqrt(FF)));
-%     plot([6 2e2], [1.5e-1 2e0]);
-%     plot([6 2e2], [1.5e-1 2e0]*10);
-% %     axis([0 6e2 0 5e1]);
-%     title('normal')
-%     legend('sqrt(FF)', 'sqrt(sqrt(FF))', 'article 1', 'article 2');
-
-%     figure;
-%     loglog(svec, F);
-%     axis([5 6e2 1e-1 5e1])
-%     x = log10(svec);
-%     y = 2*log10(FF);
-%     figure; plot(x,y);
-%     p = polyfit(x,y,1)
-%     p = polyfit(y,x,1)
-%     axis([5 6e2 1e-1 5e1])
-    
-    % ret = [ix' ix2' iy' iy2'];
+    legend('log10(sqrt(FF))', 'article');
 end
